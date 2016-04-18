290107975569627f34ed176e1a348f7dfa64b4a1
Hara Kang <hara0115@gmail.com>
2016-04-19 03:37:25 +0900

    add console.log statements
    Signed-off-by: Hara Kang <hara0115@gmail.com>

--- routes/webhook.js
+++ routes/webhook.js
@@ -11,15 +11,18 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
   console.log(req.body.entry);
   if (req.body.entry && req.body.entry.length == 0) {
+    console.log("entry is empty...");
     return res.sendStatus(500);
   }
   messaging_events = req.body.entry[0].messaging;
+  console.log("message length: " + messaging_events.length);
   for (i = 0; i < messaging_events.length; i++) {
     event = req.body.entry[0].messaging[i];
     sender = event.sender.id;
+    console.log("sender: " + sender);
     if (event.message && event.message.text) {
       text = event.message.text;
-      console.loog(text);
+      console.log("text: " + text);
       // Handle a text message from this sender
     }
   }
